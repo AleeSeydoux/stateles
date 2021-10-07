de96f1d20124f4256d006e53fa905440ebcf3457
Mayra Osorio <aleevelarde9@gmail.com>
2021-10-07 00:57:58 -0500

    componente iniciado

---
+++ src/componentes/Aplicacion.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Aplicacion extends Component{
+    render() {
+        return (
+            <div>
+                <p>Hola desde el componente Aplicacion</p>
+            </div>
+        )
+    }
+}
+
+export default Aplicacion
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Aplicacion from './componentes/Aplicacion';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Aplicacion />
   </React.StrictMode>,
   document.getElementById('root')
 );
